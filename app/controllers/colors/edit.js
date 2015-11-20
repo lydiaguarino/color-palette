d9818840dfae86ee1f8fc64f9ea8e0ad1ab8cc17
Lydia Guarino <lydia@frontside.io>
2015-11-19 18:15:50 -0600

    EIGHT Add Edit Template and Actions

--- app/controllers/colors/edit.js
+++ app/controllers/colors/edit.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    update: function(){
+      if(this.get('model.hexcode')){
+        var color = this.get('model');
+        color.save();
+        this.transitionToRoute('colors.index');
+      }
+    },
+    destroy: function(){
+      var color = this.get('model');
+      color.destroyRecord();
+      color.save();
+      this.transitionToRoute('colors.index');
+    }
+  }
 });
