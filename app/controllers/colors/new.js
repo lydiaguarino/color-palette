4ff2ee6e17395b6f8a943d9fb5424d185a1a8973
Lydia Guarino <lydia@frontside.io>
2015-11-19 18:01:21 -0600

    SEVEN Add Create Action

--- app/controllers/colors/new.js
+++ app/controllers/colors/new.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  hexcode: null,
+  name: null,
+  resetForm: function(){
+    this.setProperties({
+      hexcode: null,
+      name: null
+    });
+  },
+  actions: {
+    create: function(){
+      var colorData = {
+        hexcode: this.get('hexcode'),
+        name: this.get('hexcode')
+      };
+
+      if(this.get('hexcode')){
+        var newColor = this.store.createRecord('color', colorData );
+        newColor.save();
+        this.resetForm();
+        this.transitionToRoute('colors.index');
+      }
+    }
+  }
 });
